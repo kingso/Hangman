479ba48bac2859c2228122ca51487b63d7136c26
Kingsley Oliver <kingsleyoliver@gmail.com>
2024-10-18 12:52:59 +0100

    Updates

--- main.py
+++ main.py
@@ -1,43 +1,113 @@
-import string
-from words import words
 import random
+from words import words
 
-alphabet = set(string.ascii_uppercase)
-letters = {''}
-picked = ''
-goes = 0
+def main():
+    print("PLAY HANGMAN!")
+    game_over = False
+    current_word = random.choice(words) 
+    guessed_letters = set()
+    wrong_guesses = 0
+    
 
+    while not game_over:
+        if wrong_guesses > 6:
+            print_stand(wrong_guesses)
+            print("Sorry, you've run out of guesses. The word was:", current_word)
+            game_over = True
+            continue
+        print_stand(wrong_guesses)
+        print("Guessed letters:", ", ".join(sorted(list(guessed_letters))))
+        for letter in current_word:
+            if letter in list(guessed_letters):
+                print(letter, end=" ")
+            else:
+                print("_", end=" ")
+        print("")
+        guess = input("Enter a letter: ").lower()
+        if guess in guessed_letters:
+            print("You've already guessed that letter!")
+            continue
+        
+        if guess.isalpha() and len(guess) == 1:
+            guessed_letters.add(guess)
+            if guess in current_word:
+                
+                print(f"Good guess! The word contains the letter '{guess}'.")
+                set_test = set(current_word) & guessed_letters
+                if set(current_word) == set_test:
+                    print("Congratulations! You guessed the word!")
+                    game_over = True
+                    break
+                continue
+            else:
+                print(f"Sorry, the word does not contain the letter '{guess}'.")
+                wrong_guesses += 1
+                continue
+        else:
+            print("Invalid input! Please enter a single letter.")
+    
+    print("Game over! The word was:", current_word)
 
-def pick_word():
-    rnd = random.randint(1, len(words))
-    pick = words[rnd]
-    return pick
+def print_stand(wrong_guesses = 0):
+    top = ("___________", 
+             " |    |    ")
+    body = {0: " |    O ",
+            
+            1: " |    | ", 
+            2: " |   /| ", 
+            3: " |   /|\\", 
 
+            4: " |    | ", 
 
-picked = pick_word()
-guessed = ['_'] * (len(picked))
+            5: " |   /  ",
+            6: " |   /\\ "}
+    bottom = ("----------")
+    for i in top:
+        print(i)
+    for i in range(7):
+        match wrong_guesses:
+            case 1:
+                print(body[0])
+                break
+            case 2:
+                print(body[0])
+                print(body[1])
+                break
+            case 3:
+                print(body[0])
+                print(body[2])
+                break
+            case 4:
+                print(body[0])
+                print(body[3])
+                break
+            case 5:
+                print(body[0])
+                print(body[3])
+                print(body[4])
+                break
+            case 6:
+                print(body[0])
+                print(body[3])
+                print(body[4])
+                print(body[5])
+                break
+            case 7:
+                print(body[0])
+                print(body[3])
+                print(body[4])
+                print(body[6])
+                print(" | ")
+                break
+        
+    for i in range(1, 8 - wrong_guesses):
+        print(" | ")
 
-print('Welcome to Hangman!')
-print(f'Your word is {len(picked)} letters long, good luck!')
+    
+    print(bottom)
+        
+    
 
-while '_' in guessed:
-    goes += 1
-    print(''.join(guessed))
-    print('')
-    letter = input('Please type a letter: ').upper()
+if __name__ == "__main__":
+    main()
 
-    if letter in letters:
-        print('You\'ve already tried that letter!')
-
-    if (letter in picked) and (letter not in letters):
-        print('Well done, that letter is in the word!')
-        for i, a in enumerate(picked):
-            if a == letter:
-                guessed[i] = letter
-
-    letters.add(letter)
-    print('Letters: ', ''.join(sorted(letters)))
-
-print(
-    f'Well done, you\'ve guessed the word {picked} correctly, it took you {goes} goes!'
-)
